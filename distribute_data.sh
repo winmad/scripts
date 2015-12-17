b1b65b5885a37a15e81615186ef733886fae86a8
winmad <winmad.wlf@gmail.com>
2015-12-16 16:21:26 -0800

    change cluster size

--- distribute_data.sh
+++ distribute_data.sh
@@ -1,6 +1,6 @@
 #~/bin/bash
 
-for i in $(seq -f "%03g" 1 1)
+for i in $(seq -f "%03g" 1 20)
 do
     ssh node$i 'cd /home/ubuntu/; python distribute_data_single.py gabardine_albedo.tar.gz /mnt/gabardine/'
 done
--- run_mtssrv.sh
+++ run_mtssrv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq -f "%03g" 1 1)
+for i in $(seq -f "%03g" 1 20)
 do
     ssh node$i 'cd /home/ubuntu/; screen -d -m ./run_mtssrv_single.sh'
 done
