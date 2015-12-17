ef983b0100195035ad8621f16fcc9ec3b3868f8f
winmad <winmad.wlf@gmail.com>
2015-12-16 17:33:22 -0800

    modify fetch_data

--- fetch_data.py
+++ fetch_data.py
@@ -4,9 +4,9 @@ cmd = "s3cmd get s3://mitsuba/%s ./" % (sys.argv[1])
 print cmd
 os.system(cmd)
 
-cmd = "tar zxvf %s" % (sys.argv[1])
-print cmd
-os.system(cmd)
+#cmd = "tar zxvf %s" % (sys.argv[1])
+#print cmd
+#os.system(cmd)
 
 
 
